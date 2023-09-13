f26e9d66c43451194030b912c7a70a52519960e3
AdrianoCoutinho <adriano.98@outlook.com>
2023-09-13 16:41:00 -0300

    receba!

--- index.js
+++ index.js
@@ -4,10 +4,51 @@ const search = async () => {
     try {
         const result  = await axios.get('https://api.adviceslip.com/advice')
         const message = (result.data.slip.advice)
-        advice.innerHTML = message
+        const options = {
+            method: "POST",
+            url: "https://api.edenai.run/v2/translation/automatic_translation",
+            headers: {
+              authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiOTFmMjYzMmUtYmYwOS00MDk5LWFhZDYtNWI4ZDNiMTc3Y2E2IiwidHlwZSI6ImFwaV90b2tlbiJ9.UoF8wXN1gbpn4kPb7IUxX8y709PucOkcNLF-y8ANtPs",
+            },
+            data: {
+              providers: "amazon,google,ibm,microsoft",
+              text: message,
+              source_language: "en",
+              target_language: "pt-br",
+            },
+          };
+          const response = await axios.request(options);
+          advice.innerHTML = response.data.ibm.text;
+          console.log(response.data.ibm.text);
         return result
     } catch (error) {
         console.log(error)
     }
 }
+
+// Obtenha referências para o elemento de áudio e o botão
+const audioPlayer = document.getElementById("audioPlayer");
+const playButton = document.getElementById("playButton");
+
+// Adicione um evento de clique ao botão
+playButton.addEventListener("click", function () {
+  if (audioPlayer.paused) {
+    // Se o áudio estiver pausado, inicie a reprodução
+    audioPlayer.play();
+    playButton.textContent = "Pause";
+  } else {
+    // Se o áudio estiver reproduzindo, pause a reprodução
+    audioPlayer.pause();
+    playButton.textContent = "Play";
+  }
+});
+
+// Adicione um evento de término de reprodução para redefinir o botão para "Play"
+audioPlayer.addEventListener("ended", function () {
+  playButton.textContent = "Play";
+});
+
+
+
+
 
